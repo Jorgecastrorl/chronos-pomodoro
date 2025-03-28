95d8ec315335626776f5dc02aeb0b5b0b04bafed
Jorge Castro <jorgecrleite@hotmail.com>
2025-03-28 18:33:44 -0300

    Configurando Heading e Colocando icone

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
 
 import "./styles/themes.css";
 import "./styles/global.css";
@@ -6,7 +7,12 @@ import "./styles/global.css";
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá Mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi
         aspernatur, neque atque, laboriosam nihil veniam eius rerum suscipit
