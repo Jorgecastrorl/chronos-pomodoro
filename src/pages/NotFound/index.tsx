c7d6cc85cc08177b6c5ecab6b4965fff7bf33714
Jorge Castro <101066903+Jorgecastrorl@users.noreply.github.com>
2025-04-05 14:28:57 -0300

    Criando template de paginas

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          laboriosam corporis illo tenetur neque doloremque. Modi nam blanditiis
+          sit fuga.
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
