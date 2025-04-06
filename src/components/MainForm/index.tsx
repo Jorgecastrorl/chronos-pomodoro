6ad7c3d51602c6662fbe6294b525235f2b5d2d0b
Jorge Castro <101066903+Jorgecastrorl@users.noreply.github.com>
2025-04-06 11:26:42 -0300

    Criado Contexts

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -16,7 +16,7 @@ export function MainForm() {
       </div>
 
       <div className="formRow">
-        <p>Lorem ipsum dolor sit amet.</p>
+        <p>O proximo intervalo é de 25 min</p>
       </div>
 
       <div className="formRow">
