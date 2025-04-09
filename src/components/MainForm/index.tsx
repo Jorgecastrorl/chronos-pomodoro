b61d1bdad6e74a848d7d9ee2c5fab0b22143321f
Jorge Castro <101066903+Jorgecastrorl@users.noreply.github.com>
2025-04-09 19:32:18 -0300

    Pegando evento submir do formulario

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,17 +1,25 @@
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { PlayCircleIcon } from "lucide-react";
 import { DefaultButton } from "../DefaultButton";
 import { Cycles } from "../Cycles";
 import { DefaultInput } from "../DefaultInput";
+import { useRef } from "react";
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("Deu Certo!");
+  }
+
   return (
-    <form className="form" action="">
+    <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
         <DefaultInput
           labelText="task"
           id="meuInput"
           type="text"
           placeholder="Digite Algo"
+          ref={taskNameInput}
         />
       </div>
 
@@ -25,7 +33,6 @@ export function MainForm() {
 
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} color="green" />
-        <DefaultButton icon={<StopCircleIcon />} color="red" />
       </div>
     </form>
   );
